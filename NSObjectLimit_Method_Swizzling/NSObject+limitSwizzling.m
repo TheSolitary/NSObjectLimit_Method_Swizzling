8e26f03006d4cf48e39c10cf9f9df150c671e8e6
yimeng zhang <53313190@qq.com>
2015-03-06 23:05:05 +0800

    添加了相关注释

--- NSObjectLimit_Method_Swizzling/NSObject+limitSwizzling.m
+++ NSObjectLimit_Method_Swizzling/NSObject+limitSwizzling.m
@@ -13,8 +13,24 @@ static NSString *authorizeIdentifier = @"com.threeti.NSObjectLimit-Method-Swizzl
 
 @implementation NSObject (limitSwizzling)
 
+/**
+ *  我们可以利用method_exchangeImplementations 来交换2个方法中的IMP，
+ *  我们可以利用 class_replaceMethod 来修改类，
+ *  我们可以利用 method_setImplementation 来直接设置某个方法的IMP，
+ *  归根结底，都是偷换了selector的IMP
+ */
+
 +(void)load
 {
+    
+    /**
+     *  Swizzling 的处理，在类的 +load 方法中完成。
+     *  因为 +load 方法会在类被添加到 OC 运行时执行，保证了 Swizzling 方法的及时处理。
+     */
+    
+    /**
+     *  Swizzling 的处理，dispatch_once 中完成。保证只执行一次。
+     */
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         Class class = [self class];
@@ -24,6 +40,8 @@ static NSString *authorizeIdentifier = @"com.threeti.NSObjectLimit-Method-Swizzl
         
         Method originalMethod = class_getInstanceMethod(class, originalSelector);
         Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        //IMP实际上是一个函数指针，指向方法实现的首地址
         
         BOOL didAddMehod = class_addMethod(class,
                                            originalSelector,
@@ -48,12 +66,14 @@ static NSString *authorizeIdentifier = @"com.threeti.NSObjectLimit-Method-Swizzl
     const char *currentClassName_char = [currentClassName UTF8String];
     __weak id currentClass = objc_getClass(currentClassName_char);
     
+    //遍历当前我们需要授权的类的名字的数组
     [[self limitClassArray] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        //获取到“受保护”类的对象
         const char *limitClassName_char = [(NSString *)obj UTF8String];
         id limitClass = objc_getClass(limitClassName_char);
         
         if (limitClass == currentClass) {
-            
+            //获取当前identifier
             NSString *appIdentifier = [[NSBundle mainBundle] bundleIdentifier];
             //NSAssert([appIdentifier isEqualToString:authorizeIdentifier], @"当前bundleIdentifier不匹配，请联系管理员进行授权");
             
